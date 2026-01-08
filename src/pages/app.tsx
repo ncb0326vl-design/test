4040b2846aebd151034baef278179aabfcd13737
Vince Liao <vince.liao@nextbank.com.tw>
2026-01-08 17:17:42 +0800

    app

---
+++ src/pages/app.tsx
@@ -0,0 +1,85 @@
+import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
+
+const App = ({ domain }: {domain: string}) => {
+  const { query } = useRouter();
+  const { deep, auto, invalid, action } = query;
+
+  const deepLink = useMemo(() => {
+    const subdomain = domain || 'www';
+    let url = `${subdomain}.nextbank.com.tw/app`;
+    if (action) {
+      url += `?action=${action}`;
+    }
+    return url;
+  }, [action, domain]);
+
+  const userAgent = navigator.userAgent.toLowerCase();
+  const isIOS = /iphone|ipad|ipod/.test(userAgent);
+  const isAndroid = /android/.test(userAgent);
+
+  useEffect(() => {
+    if (auto === '1' && isAndroid) {
+      console.log(`used href intent://${deepLink}#Intent;scheme=https;package=com.nextbank.ncbportal;end`)
+      window.location.href = `intent://${deepLink}#Intent;scheme=https;package=com.nextbank.ncbportal;end`;
+    }
+  }, [auto, deepLink]);
+
+
+  const href = useMemo(() => {
+    if (isIOS) {
+      return `https://${deepLink}&invalid=1`;
+    }
+
+    if (isAndroid) {
+      const fallbackUrl = encodeURIComponent(`https://${deepLink}&invalid=1`);
+      return `intent://${deepLink}#Intent;scheme=https;package=com.nextbank.ncbportal;S.browser_fallback_url=${fallbackUrl};end`;
+    }
+
+    return `https://${deepLink}&invalid=1`;
+  }, [isAndroid, isIOS, deepLink]);
+
+  return <div>
+    {invalid === '1' || deep !== '1' ? (
+      <div>
+        App下載頁面
+       </div>
+      ) : (
+        <div
+        style={{
+          width: '100%',
+          padding: '60px',
+          minHeight: '500px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <a
+          href={href}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            padding: '20px 30px',
+            background: 'yellow',
+            height: '100px',
+            textDecoration: 'none',
+          }}
+        >
+          開啟app
+        </a>
+      </div>
+      )}
+  </div>
+};
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { domain } = context.query;
+  return {
+    props: {
+      domain: domain || null,
+    },
+  };
+};
+
+export default App;
--- src/pages/test-client-side-android.tsx
+++ src/pages/test-client-side-android.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function TestClientSide() {
   useEffect(() => {
-    window.location.href = `intent://www.nextbank.com.tw/app#Intent;scheme=https;package=com.nextbank.ncbportal;S.browser_fallback_url=${encodeURIComponent("https://www.nextbank.com.tw/app")};end`;
+    window.location.href = `intent://www.nextbank.com.tw/app#Intent;scheme=https;package=com.nextbank.ncbportal;end`;
   }, []);
 
   return null;
